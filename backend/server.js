1e7675e5d9672865a3d60f6c3b2dbb8565a84a48
Pachara Auikim <6630207321@student.chula.ac.th>
2025-05-21 15:33:43 +0700

    can connect to ios mobile

--- backend/server.js
+++ backend/server.js
@@ -48,4 +48,4 @@ app.delete('/tasks/:id', (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
